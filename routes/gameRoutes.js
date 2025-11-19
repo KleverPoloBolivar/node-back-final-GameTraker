fcb7f98eacf2a9403ed9b53c107ad0aaef8d2f7a
KleverPoloBolivar <kleverpolobolivar@gmail.com>
2025-11-18 22:37:12 -0500

    Bug: Arreglo al mostrar interfaz, conexion con el backEnd y boton juego completado o pendiente

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { getGames, createGame, deleteGame } = require("../controllers/gameController");
+
+const { 
+  getGames, 
+  createGame, 
+  deleteGame,
+  updateGame
+} = require("../controllers/gameController");
 
 router.get("/", getGames);
 router.post("/", createGame);
 router.delete("/:id", deleteGame);
 router.put("/:id", updateGame);
+
 module.exports = router;
